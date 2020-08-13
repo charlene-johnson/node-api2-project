013365f9927eb0a488f096b9d5ed2294fa7e3a54
charlene-johnson <charlene.j8234@gmail.com>
2020-08-13 19:42:47 -0400

    Added .env and welcome router

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express")
 const postsRouter = require("./posts/posts-router")
+const welcomeRouter = require("./welcome-router")
 
 
 const server = express()
-const port = 4000
+const port = process.env.PORT || 4000
 
 
 
 
 server.use(express.json())
+server.use("/", welcomeRouter)
 server.use(postsRouter)
 
 server.listen(port, () => {
